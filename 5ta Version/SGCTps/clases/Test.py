7c90cce61199503d89553c95bd6c04c3edbb2b96
Skruptovski <solid_reaper14@yahoo.com.ar>
2017-10-23 11:25:31 -0300

    CrearTP+carga de una actividad

--- 5ta Version/SGCTps/clases/Test.py
+++ 5ta Version/SGCTps/clases/Test.py
@@ -1,4 +1,5 @@
 from Admin import Admin
+from Usuario import Usuario
 from dbpython import *
 
 sql = "SELECT usuario from usuarios WHERE usuario='SMartins'"
@@ -7,5 +8,17 @@ row = cursor.fetchone()
 usuario = str(row[0])
 admin = Admin(usuario)
 #CARGAR UN REGISTRO DE TIPO USUARIO----------
-admin.crearUsuario("Diego", "Saavedra", "Skribano", "RojoLocura")
+admin.crearUsuario("Fabricio", "Bustos", "Best4ARG", "CAI")
+#CARGAR UN REGISTRO DE TIPO TP----------
+sql = "SELECT * from usuarios WHERE usuario='Skribano'"
+cursor.execute(sql)
+row = cursor.fetchone()
+idUsuario = str(row[0])
+nombre = str(row[1])
+apellido = str(row[2])
+usuario = str(row[3])
+clave = str(row[4])
+user = Usuario(idUsuario, nombre, apellido, usuario, clave)
+user.crearTrabajoPractico()
+
 
--- 5ta Version/SGCTps/clases/Usuario.py
+++ 5ta Version/SGCTps/clases/Usuario.py
@@ -1,13 +1,18 @@
 class Usuario:
 #CONSTRUCTOR ---------------------
-    def __init__(self, nombre, apellido, usuario, clave, tipo):
+    def __init__(self,idUsuario, nombre, apellido, usuario, clave):
+        self.__idUsuario = idUsuario
         self.__nombre = nombre
         self.__apellido = apellido
         self.__usuario = usuario
         self.__clave = clave
-        self.__tipo = tipo
 
 #METODOS ---------------------
+    def setIdUsuario(self, idUsuario):
+        self.__idUsuario = idUsuario
+
+    def getIdUsuario(self):
+        return self.__idUsuario
 
     def setNombre(self, nombre):
         self.__nombre = nombre
@@ -33,14 +38,28 @@ class Usuario:
     def getClave(self):
         return self.__clave
 
-    def setTipo(self, tipo):
-        self.__tipo = tipo
-
-    def getTipo(self):
-        return self.__tipo
-
     def imprimirDatos(self):
+        print "IdUsuario: ", self.getIdUsuario()
         print "Nombre: ", self.getNombre()
         print "Apellido: ", self.getApellido()
         print "Usuario: ", self.getUsuario()
         print "Password: ", self.getClave()
+
+    def crearTrabajoPractico(self):
+        titulo = raw_input("Ingrese titulo del trabajo practico: ")
+        materia = raw_input("Ingrese materia del trabajo practico: ")
+        carrera = raw_input("Ingrese carrera del trabajo practico: ")
+        idUsuario = str(self.getIdUsuario())
+        from dbpython import *
+        sql = "INSERT INTO trabajos(titulo, materia, carrera, idUsuario) values('" + titulo + "', '" + materia + "', '" + carrera + "', '" + idUsuario + "')"
+        cursor.execute(sql)
+        db.commit()
+        consigna = raw_input("Detalle su ejercicio: ")
+        respuesta = raw_input("Escriba la respuesta correcta: ")
+        sql = "SELECT idTrabajo from trabajos WHERE idUsuario='" + idUsuario + "'"
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        idTrabajo = str(row[0])
+        sql = "INSERT INTO actividades(idTrabajo, consigna, respuesta) values('" + idTrabajo + "', '" + consigna + "', '" + respuesta + "')"
+        cursor.execute(sql)
+        db.commit()
